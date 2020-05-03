11fea5c919ada406709da7a8b5c698cf3829bb91
reitwiec <reitwiec@gmail.com>
2020-05-03 23:35:05 +0530

    Webdev challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Countdown from './components/Countdown';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  padding:50px;
+min-height:100vh;
+  margin-left:300px;
+  margin-right:300px;
+  .title{
+    padding:20px;
+    width:100%;
+    text-align:left;
+    margin-top:50px;
+    font-size:40px;
+    font-weight:900;
+    color:#46535E;
+    letter-spacing:2px;
+    border-bottom:2px solid #E5E7E8;
+  }
+  h2{
+    color:#46535E;
+  }
+  p{
+    font-size:25px;
+    color:#46535E;
+  }
+  li{
+    font-size:20px;
+    color:#46535E;
+    line-height:40px;
+  }
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Countdown />
+      <div className="title">
+        IECSE WEB Head and DEV Head Challenge
+      </div>
+      <div>
+        <h2 style={{ textDecoration: "underline" }}>#Prompt</h2>
+        <p style={{ fontWeight: 500, fontSize: "30px" }}>Build a Dribbble for IECSE website or an application using any of your preferred tech-stack.</p>
+        <p>You have to implement as many of the following features as possible:</p>
+        <ul>
+          <li>User Authentication</li>
+          <li>Upload designs to an S3 bucket</li>
+          <li>Allow user to add a personal bio</li>
+          <li>Feature to follow other designers</li>
+          <li>Feature to like designs posted by other designers</li>
+        </ul>
+        <p style={{ fontWeight: 500, textDecoration: "underline" }}>Bonus Points:</p>
+        <ul>
+          <li>Save other users designs</li>
+          <li>Comment on other users designs</li>
+          <li>Mention a user in the comments</li>
+          <li>Deploy your website (Heroku not allowed) / Release apk</li>
+        </ul>
+        <p style={{ fontWeight: 500, textDecoration: "underline" }}>Judgement and Advice:</p>
+        <ul>
+          <li>We don't expect a perfect end product.</li>
+          <li>Play to your strengths.</li>
+          <li>If you are more inclined towards backend development, usage of templates is allowed.</li>
+          <li>If you have any queries regarding the challenge, PM any one of us from the board.</li>
+        </ul>
+
+        <div style={{ textAlign: "center", marginTop: '50px' }}>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfLNLHSniHLNmggStJvB76AdMSbaPVWuCWSkYDl2J2miWeByw/viewform?embedded=true" width="640" height="645" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/components/Countdown.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+
+const Timer = styled.div`
+    width:100%;
+    text-align:center;
+    font-size:50px;
+    font-weight:900;
+    letter-spacing:2px;
+    color:#252C33;
+    text-transform:capitalize;
+`;
+
+export default function Countdown() {
+    const calculateTimeLeft = () => {
+        const difference = +new Date("2020-05-05T00:00:00.000+05:30") - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60)
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+    });
+
+    const timerComponents = [];
+
+    Object.keys(timeLeft).forEach(interval => {
+        if (!timeLeft[interval]) {
+            return;
+        }
+
+        timerComponents.push(
+            <span>
+                {timeLeft[interval]} {interval}{" "}
+            </span>
+        );
+    });
+
+    return (
+        <Timer>
+            {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+        </Timer>
+    );
+}
