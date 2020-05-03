6b048e3984fdb3254c13a5d6cfd3bb2254824bf5
reitwiec <reitwiec@gmail.com>
2020-05-03 23:35:52 +0530

    Updates

---
+++ precache-manifest.b18844d8e13a5c479e90c55a8269d45c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd35c20840eb9e52509a871a8ad2cde3",
+    "url": "/webdevchallenge/index.html"
+  },
+  {
+    "revision": "83e5952c190dd1b04b59",
+    "url": "/webdevchallenge/static/css/main.392b7f4f.chunk.css"
+  },
+  {
+    "revision": "0c9f8f56123044de11af",
+    "url": "/webdevchallenge/static/js/2.033174ec.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/webdevchallenge/static/js/2.033174ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e5952c190dd1b04b59",
+    "url": "/webdevchallenge/static/js/main.cf78d522.chunk.js"
+  },
+  {
+    "revision": "a3fe549e5cc6bec13f3a",
+    "url": "/webdevchallenge/static/js/runtime-main.ad710b92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webdevchallenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
